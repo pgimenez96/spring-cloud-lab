ad9b562d4154326bad5e3d23940811cbc6ba2376
Pablo Gimenez <pablo.gimenez.zorrilla@gmail.com>
2025-03-11 17:33:32 -0300

    refactor: se ajusta configuracion del swagger del componente

--- spring-cloud-root/companies/src/main/java/py/com/pgimenez/companies/configs/OpenApiConfig.java
+++ spring-cloud-root/companies/src/main/java/py/com/pgimenez/companies/configs/OpenApiConfig.java
@@ -1,16 +1,31 @@
 package py.com.pgimenez.companies.configs;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Companies CRUD",
-                version = "1.0.0",
-                description = "This is a CRUD for management companies"
-        )
-)
 public class OpenApiConfig {
+
+        @Bean
+        public OpenAPI buildOpenApi(ServletContext servletContext) {
+                return new OpenAPI()
+                        .addServersItem(new Server().url(servletContext.getContextPath()))
+                        .info(new Info()
+                                .title("Companies CRUD")
+                                .description("This is a CRUD for management companies")
+                                .version("1.0.0")
+                                .license(new License()
+                                        .name("Apache 2.0")
+                                        .url("http://springdoc.org")))
+                        .externalDocs(new ExternalDocumentation()
+                                .description("")
+                                .url(""));
+        }
+
 }
